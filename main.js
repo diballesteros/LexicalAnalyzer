2a8f073ebbf60f5650b515ac0799b726b95c31e2
Diego Ballesteros <diballesterosc@gmail.com>
2018-05-28 21:37:16 -0500

    Fixed operator regex, file manipulation, language

--- main.js
+++ main.js
@@ -1,53 +1,91 @@
-let lex = function (input) {
+function lex () {
 
-    let isOperator = function (c) { return /[+\-*\/\^%=(),]/.test(c); };
-    let isDigit = function (c) { return /[0-9]/.test(c); };
-    let isWhiteSpace = function (c) { return /\s/.test(c); };
-    let isIdentifier = function (c) { return typeof c === "string" && !isOperator(c) && !isDigit(c) && !isWhiteSpace(c); };
+    var input = document.getElementById("textoAGuardar").value;
+
+    let validarOperador = function (c) { return /[+\-*\/\^%=(),;<>{}[]/.test(c); };
+    let validarDigito = function (c) { return /[0-9]/.test(c); };
+    let validarEspacio = function (c) { return /\s/.test(c); };
+    let validarVariable = function (c) { return typeof c === "string" && !validarOperador(c) && !validarDigito(c) && !validarEspacio(c); };
 
     let palabras = ['for', 'var', 'while', 'if', 'boolean', 'double', 'false', 'true', 'null'];
 
-    var tokens = [], c, i = 0;
-    var advance = function () { return c = input[++i]; };
-    var addToken = function (type, value) {
+    var tokens = []
+    var c = 0;
+    var i = 0;
+
+    var avanzar = function () { return c = input[++i]; };
+    var agregarToken = function (token, valor) {
         tokens.push({
-            type: type,
-            value: value
+            token: token,
+            valor: valor
         });
     };
     while (i < input.length) {
         c = input[i];
-        if (isWhiteSpace(c)) {
-            advance();
+        if (validarEspacio(c)) {
+            avanzar();
         }
-        else if (isOperator(c)) {
-            addToken("operador", c);
-            advance();
+        else if (validarOperador(c)) {
+            agregarToken("operador", c);
+            avanzar();
         }
-        else if (isDigit(c)) {
+        else if (validarDigito(c)) {
             let num = c;
-            while (isDigit(advance())) num += c;
+            while (validarDigito(avanzar())) num += c;
             if (c === ".") {
-                do num += c; while (isDigit(advance()));
+                do num += c; while (validarDigito(avanzar()));
             }
             num = parseFloat(num);
-            if (!isFinite(num)) throw "Numero demasiado grande para un double de 64 bits";
-            addToken("numero", num);
+            agregarToken("numero", num);
         }
-        else if (isIdentifier(c)) {
+        else if (validarVariable(c)) {
             let idn = c;
-            while (isIdentifier(advance())) idn += c;
+            while (validarVariable(avanzar())) idn += c;
             if (palabras.includes(idn)) {
-                addToken("palabra reservada", idn)
+                agregarToken("palabra reservada", idn)
             } else {
-                addToken("variable", idn);
+                agregarToken("variable", idn);
             }
         }
         else throw "No reconocido";
 
     }
-    addToken("(end)");
+    console.log(tokens)
     return tokens;
 };
 
-console.log(lex('for(var i = 0; i<10;i++){x=x+1}'))
+function guardarTextoComoArchivo()
+{
+    var texto = document.getElementById("textoAGuardar").value;
+    var textoBlob = new Blob([texto], {type:"text/plain"});
+    var textoURL = window.URL.createObjectURL(textoBlob);
+    var nombreArchivoAGuardar = document.getElementById("nombreArchivoAGuardar").value;
+ 
+    var linkDescarga = document.createElement("a");
+    linkDescarga.download = nombreArchivoAGuardar;
+    linkDescarga.innerHTML = "Descargar Archivo";
+    linkDescarga.href = textoURL;
+    linkDescarga.onclick = destruirElemento;
+    linkDescarga.style.display = "none";
+    document.body.appendChild(linkDescarga);
+ 
+    linkDescarga.click();
+}
+ 
+function destruirElemento(event)
+{
+    document.body.removeChild(event.target);
+}
+ 
+function cargarArchivo()
+{
+    var archivoCargado= document.getElementById("archivoACargar").files[0];
+ 
+    var fileReader = new FileReader();
+    fileReader.onload = function(event) 
+    {
+        var textoCargado = event.target.result;
+        document.getElementById("textoAGuardar").value = textoCargado;
+    };
+    fileReader.readAsText(archivoCargado, "UTF-8");
+}
